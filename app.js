9535e246e78bda37dc043865f64c06e2d88f0bd7
Jordan Starr <jordanstarr@Jordans-MacBook-Pro.local>
2019-11-10 15:41:32 -0800

    un-minify js and css files

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var videoList=[];const INSERT_VIDEO_COUNT_HERE=50,NUMBER_OF_VIDEOS=51;for(var video=1;video<51;video++)videoList.push("https://d9m01xi7ip4je.cloudfront.net/Videos/"+video+".mp4");var randomVideo=videoList[Math.floor(Math.random()*videoList.length)],pickaVideo=function(){return'<video class="fullscreen-video" loop muted autoplay poster="'+randomVideo+'"><source src="'+randomVideo+'" type="video/mp4"></video>'};document.getElementById("arrayString").innerHTML=pickaVideo(),chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSeykxJbhQckDZ1j3WU3D8Onr06uliiABdhtc1aIW6mxjzBCfQ/viewform?usp=sf_link");
+var videoList = [];
+const INSERT_VIDEO_COUNT_HERE = 50,
+    NUMBER_OF_VIDEOS = 51;
+for (var video = 1; video < 51; video++) videoList.push("https://d9m01xi7ip4je.cloudfront.net/Videos/" + video + ".mp4");
+var randomVideo = videoList[Math.floor(Math.random() * videoList.length)],
+    pickaVideo = function() {
+        return '<video class="fullscreen-video" loop muted autoplay poster="' + randomVideo + '"><source src="' + randomVideo + '" type="video/mp4"></video>'
+    };
+document.getElementById("arrayString").innerHTML = pickaVideo(), chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSeykxJbhQckDZ1j3WU3D8Onr06uliiABdhtc1aIW6mxjzBCfQ/viewform?usp=sf_link");
--- googAnalytics.js
+++ googAnalytics.js
@@ -1,1 +1,8 @@
-var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-117531599-1"]),_gaq.push(["_trackPageview"]),function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://ssl.google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+var _gaq = _gaq || [];
+_gaq.push(["_setAccount", "UA-117531599-1"]), _gaq.push(["_trackPageview"]),
+    function() {
+        var e = document.createElement("script");
+        e.type = "text/javascript", e.async = !0, e.src = "https://ssl.google-analytics.com/ga.js";
+        var t = document.getElementsByTagName("script")[0];
+        t.parentNode.insertBefore(e, t)
+    }();
