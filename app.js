f17875d9682447a863203674ae1205a63087cfc3
Jordan Starr <jordanstarr@Jordans-MacBook-Pro.local>
2019-11-12 23:40:52 -0800

    Added support for offline videos, enabled video play on as small as 500px screen widths, updated video endpoints to AWS Cloudfront to reduce latency, refactored css, and added 5 new videos

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+/*********************************************
+Replace the main html page with a random video
+**********************************************/
+//1. Create an array of videos that map to the video endpoints in AWS 
 var videoList = [];
-const INSERT_VIDEO_COUNT_HERE = 50,
-    NUMBER_OF_VIDEOS = 51;
-for (var video = 1; video < 51; video++) videoList.push("https://d9m01xi7ip4je.cloudfront.net/Videos/" + video + ".mp4");
-var randomVideo = videoList[Math.floor(Math.random() * videoList.length)],
-    pickaVideo = function() {
-        return '<video class="fullscreen-video" loop muted autoplay poster="' + randomVideo + '"><source src="' + randomVideo + '" type="video/mp4"></video>'
-    };
-document.getElementById("arrayString").innerHTML = pickaVideo(), chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSeykxJbhQckDZ1j3WU3D8Onr06uliiABdhtc1aIW6mxjzBCfQ/viewform?usp=sf_link");
+const INSERT_VIDEO_COUNT_HERE = 56 
+const NUMBER_OF_VIDEOS = INSERT_VIDEO_COUNT_HERE + 1; 
+for (var video=1; video<NUMBER_OF_VIDEOS; video++) { 
+	videoList.push("https://d9m01xi7ip4je.cloudfront.net/Videos/"+video+".mp4");
+}
+
+//2. Select a random video from the list & return the random video number wrapped in HTML content to autoplay the video in a loop
+var onlineRandomVideo = videoList[Math.floor(Math.random() * videoList.length)];
+var pickaVideo = function(online_or_offline) {
+	var HtmlVideoString = "<video class=\"fullscreen-video\" loop muted autoplay poster=\"" + online_or_offline + "\"><source src=\"" + online_or_offline + "\" type=\"video/mp4\"></video>";
+	// console.log(online_or_offline) //Uncomment for testing purposes to display what video is selected.
+	return HtmlVideoString;
+};
+
+//3. Replace the innerHTML of the main page div with the randomly chosen video
+if (window.navigator.onLine == true){
+	document.getElementById("arrayString").innerHTML=pickaVideo(onlineRandomVideo);
+} else {
+	offlineVideo = "offlineCat.mp4"
+	document.getElementById("arrayString").innerHTML=pickaVideo(offlineVideo);	
+}
+
+/*********************************************
+Redirect users to a feedback form on uninstall
+**********************************************/
+chrome.runtime.setUninstallURL('https://docs.google.com/forms/d/e/1FAIpQLSeykxJbhQckDZ1j3WU3D8Onr06uliiABdhtc1aIW6mxjzBCfQ/viewform?usp=sf_link');
